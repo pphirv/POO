a78e0b0e5f129473720a5418e8bfab72d7c6dae1
dievazq <dievazq@PackardBell>
2015-10-19 12:34:15 +0200

    Inicio del proyecto

---
+++ dievazq-rodpere/src/academia/Academia.java
@@ -0,0 +1,10 @@
+package academia;
+
+public class Academia {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ dievazq-rodpere/src/academia/Alumno.java
@@ -0,0 +1,5 @@
+package academia;
+
+public class Alumno {
+
+}
